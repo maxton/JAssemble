abe1c08a881e28a8a65903647dcb221a352579b9
maxton <maxton@maxton.xyz>
2015-11-08 18:41:32 -0500

    Add more CPU tests

--- test/jassemble/CPUTest.java
+++ test/jassemble/CPUTest.java
@@ -25,7 +25,7 @@ public class CPUTest {
   }
   
   @Test
-  public void TestSequence1() throws Exception {
+  public void TestAddiMove() throws Exception {
     this.data = new byte[256];
     this.as = new Assembler("li $0, 3\n"
             + "addi $0, $0, 4\n"
@@ -41,5 +41,110 @@ public class CPUTest {
     assertEquals(7, instance.getRegister(3));
   }
 
+  @Test
+  public void TestSll() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 3\n"
+                          + "sll $0, $0, 4");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(3);
+    assertEquals(48, instance.getRegister(0));
+  }
   
+  @Test
+  public void TestSra() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 48\n"
+                          + "sra $0, $0, 4");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(3);
+    assertEquals(3, instance.getRegister(0));
+  }
+  
+  @Test
+  public void TestClr() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 48\n"
+                          + "clr $0");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(3);
+    assertEquals(0, instance.getRegister(0));
+  }
+  
+  @Test
+  public void TestMemoryInstructions() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 48\n"
+                          + "sw $0, 2($1)\n"
+                          + "lw $2, 2($1)");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(4);
+    assertEquals(48, data[2]);
+    assertEquals(0, instance.getRegister(1));
+    assertEquals(48, instance.getRegister(2));
+  }
+  
+  @Test
+  public void TestAndAndi() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 8\n"
+                          + "and $1, $0, $1\n"
+                          + "andi $0, $2, 15");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(4);
+    assertEquals(8, instance.getRegister(0));
+    assertEquals(0, instance.getRegister(1));
+    assertEquals(8, instance.getRegister(2));
+  }
+  
+  @Test
+  public void TestOrOri() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 8\n"
+                          + "or $1, $0, $1\n"
+                          + "ori $0, $2, 15");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(4);
+    assertEquals(8, instance.getRegister(0));
+    assertEquals(8, instance.getRegister(1));
+    assertEquals(15, instance.getRegister(2));
+  }
+  
+  @Test
+  public void TestInv() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 8\n"
+                          + "inv $1, $0\n"
+                          + "inv $2, $1");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(4);
+    assertEquals(8, instance.getRegister(0));
+    assertEquals(-9, instance.getRegister(1));
+    assertEquals(8, instance.getRegister(2));
+  }
+  
+  @Test
+  public void TestBranch() throws Exception {
+    this.data = new byte[256];
+    this.as = new Assembler("li  $0, 8\n"
+                          + "bne $1, $0, label\n"
+                          + "inv $2, $1\n"
+                          + "label: clr $0\n"
+                          + "beq $1, $0, label2\n"
+                          + "inv $2, $1\n"
+                          + "label2:");
+    as.assemble();
+    CPU instance = new CPU(as.getInstructionWords(), data);
+    instance.step(8);
+    assertEquals(0, instance.getRegister(0));
+    assertEquals(0, instance.getRegister(1));
+    assertEquals(0, instance.getRegister(2));
+  }
 }
